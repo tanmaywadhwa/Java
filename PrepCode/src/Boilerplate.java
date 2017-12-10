a0e1ae2f70b4f8cef0f7736f062011c60d4f8454
Tanmay Wadhwa <wadhwa.tanmay@gmail.com>
2017-12-10 11:30:09 -0800

    minor changes :)

---
+++ PrepCode/src/Boilerplate.java
@@ -0,0 +1,9 @@
+class Boilerplate{
+	public static void main(String args[]){
+		System.out.println(method());
+	}
+	
+	public static String method(){
+		return "true";
+	}
+}
--- PrepCode/src/HelloWorld.java
+++ PrepCode/src/HelloWorld.java
@@ -1,3 +1,4 @@
+// Just a Hello World App :)
 class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World");
